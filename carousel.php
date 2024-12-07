bdf595802843de69dfbf19a08b1b113e49ca2ab5
BISHWAS PAUDEL <102367831+Bishwas-paudel@users.noreply.github.com>
2024-12-07 19:05:10 +0545

    Update carousel.php

--- carousel.php
+++ carousel.php
@@ -4,6 +4,9 @@
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
     </ol>
     <div class="carousel-inner">
+
+
+      
       <div class="item active">
         <img src="images/carousel.png" alt="RentHouse" style="width:100%; height: 50%;">
       </div>
